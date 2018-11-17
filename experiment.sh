c7744666c9c144fe66fea2755334eab0241b3f04
AkarshG1 <akarsh98@gmail.com>
2018-11-17 22:33:27 +0100

    changed shell script to train on pong

--- experiment.sh
+++ experiment.sh
@@ -5,9 +5,14 @@
 #echo "Training acktr on SpaceInvaders for 10M Steps"
 #OPENAI_LOGDIR=logs/si_10M_acktr python -m baselines.run --alg=acktr --env=SpaceInvadersNoFrameskip-v0 --num_timesteps=1e7 --save_path=models/si_10M_acktr 
 
-echo "Training ppo2 on Breakout for 10M Steps"
-OPENAI_LOGDIR=logs/bo_10M_ppo2 python -m baselines.run --alg=ppo2 --env=BreakoutNoFrameskip-v0 --num_timesteps=1e7 --save_path=models/bo_10M_ppo2 
+#echo "Training ppo2 on Breakout for 10M Steps"
+#OPENAI_LOGDIR=logs/bo_10M_ppo2 python -m baselines.run --alg=ppo2 --env=BreakoutNoFrameskip-v0 --num_timesteps=1e7 --save_path=models/bo_10M_ppo2 
 
 #echo "Training acktr on Breakout for 10M Steps"
 #OPENAI_LOGDIR=logs/bo_10M_acktr python -m baselines.run --alg=acktr --env=BreakoutNoFrameskip-v0 --num_timesteps=1e7 --save_path=models/bo_10M_acktr 
 
+echo "Training ppo2 on Pong for 10M Steps"
+OPENAI_LOGDIR=logs/pong_10M_ppo2 python -m baselines.run --alg=ppo2 --env=PongFrameskip-v0 --num_timesteps=1e7 --save_path=models/pong_10M_ppo2 
+
+echo "Training acktr on Pong for 10M Steps"
+OPENAI_LOGDIR=logs/pong_10M_acktr python -m baselines.run --alg=acktr --env=PongFrameskip-v0 --num_timesteps=1e7 --save_path=models/pong_10M_acktr 
