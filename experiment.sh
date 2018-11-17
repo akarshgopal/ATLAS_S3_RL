77200260d27e615aa4075549eed08db359efa529
AkarshG1 <akarsh98@gmail.com>
2018-11-17 16:47:35 +0100

    trained ppo2 10M steps on breakout

--- experiment.sh
+++ experiment.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
-echo "Training ppo2 on SpaceInvaders for 10M Steps"
-OPENAI_LOGDIR=logs/si_10M_ppo2 python -m baselines.run --alg=ppo2 --env=SpaceInvadersNoFrameskip-v0 --num_timesteps=1e7 --save_path=models/si_10M_ppo2 
-echo "Training acktr on SpaceInvaders for 10M Steps"
-OPENAI_LOGDIR=logs/si_10M_acktr python -m baselines.run --alg=acktr --env=SpaceInvadersNoFrameskip-v0 --num_timesteps=1e7 --save_path=models/si_10M_acktr 
+
+#echo "Training ppo2 on SpaceInvaders for 10M Steps"
+#OPENAI_LOGDIR=logs/si_10M_ppo2 python -m baselines.run --alg=ppo2 --env=SpaceInvadersNoFrameskip-v0 --num_timesteps=1e7 --save_path=models/si_10M_ppo2 
+#echo "Training acktr on SpaceInvaders for 10M Steps"
+#OPENAI_LOGDIR=logs/si_10M_acktr python -m baselines.run --alg=acktr --env=SpaceInvadersNoFrameskip-v0 --num_timesteps=1e7 --save_path=models/si_10M_acktr 
+
+echo "Training ppo2 on Breakout for 10M Steps"
+OPENAI_LOGDIR=logs/bo_10M_ppo2 python -m baselines.run --alg=ppo2 --env=BreakoutNoFrameskip-v0 --num_timesteps=1e7 --save_path=models/bo_10M_ppo2 
+
+#echo "Training acktr on Breakout for 10M Steps"
+#OPENAI_LOGDIR=logs/bo_10M_acktr python -m baselines.run --alg=acktr --env=BreakoutNoFrameskip-v0 --num_timesteps=1e7 --save_path=models/bo_10M_acktr 
 
