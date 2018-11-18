d7513784a9d94d5eeda3ca8d7f23b80624a294d3
AkarshG1 <akarsh98@gmail.com>
2018-11-18 13:24:39 +0100

    trained ppo2 10M steps on demonattack, added readme, split scripts for akarsh and leon

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # ATLAS_S3_RL
 Transfer learning experiment using OpenAI Baselines
+
+
+1.Clone the repository and create a new branch to be safe.
+2.To run shell script that calls corresponding baseline algos on a permutation of games:
+  First navigate to the clone directory of this repo, then on terminal:
+
+	$ chmod +x experiment-[insert-relevant-script-version].sh 		# eg: chmod +x experiment-leon.sh
+	$ ./experiment-[insert-relevant-script-version].sh 	
+
+3.This should create models and logs directory in the repo folder, populated with data from the training runs.
+4.The Baselines_vis.ipynb can be used to visualise the learning curves for the models. Instructions for this will be added soon.
---
+++ experiment-leon.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#echo "Training ppo2 on SpaceInvaders for 10M Steps"
+#OPENAI_LOGDIR=logs/si_10M_ppo2 python -m baselines.run --alg=ppo2 --env=SpaceInvadersNoFrameskip-v0 --num_timesteps=1e7 --save_path=models/si_10M_ppo2 
+#echo "Training acktr on SpaceInvaders for 10M Steps"
+#OPENAI_LOGDIR=logs/si_10M_acktr python -m baselines.run --alg=acktr --env=SpaceInvadersNoFrameskip-v0 --num_timesteps=1e7 --save_path=models/si_10M_acktr 
+
+#echo "Training ppo2 on Breakout for 10M Steps"
+#OPENAI_LOGDIR=logs/bo_10M_ppo2 python -m baselines.run --alg=ppo2 --env=BreakoutNoFrameskip-v0 --num_timesteps=1e7 --save_path=models/bo_10M_ppo2 
+
+#echo "Training acktr on Breakout for 10M Steps"
+#OPENAI_LOGDIR=logs/bo_10M_acktr python -m baselines.run --alg=acktr --env=BreakoutNoFrameskip-v0 --num_timesteps=1e7 --save_path=models/bo_10M_acktr 
+
+#echo "Training ppo2 on Pong for 10M Steps"
+#OPENAI_LOGDIR=logs/pong_10M_ppo2 python -m baselines.run --alg=ppo2 --env=PongNoFrameskip-v0 --num_timesteps=1e7 --save_path=models/pong_10M_ppo2 
+
+#echo "Training acktr on Pong for 10M Steps"
+#OPENAI_LOGDIR=logs/pong_10M_acktr python -m baselines.run --alg=acktr --env=PongNoFrameskip-v0 --num_timesteps=1e7 --save_path=models/pong_10M_acktr 
+
+#echo "Training ppo2 on DemonAttack for 10M Steps"
+#OPENAI_LOGDIR=logs/da_10M_ppo2 python -m baselines.run --alg=ppo2 --env=DemonAttackNoFrameskip-v0 --num_timesteps=1e7 --save_path=models/da_10M_ppo2 
+
+echo "Training acktr on DemonAttack for 10M Steps"
+OPENAI_LOGDIR=logs/da_10M_acktr python -m baselines.run --alg=acktr --env=DemonAttackNoFrameskip-v0 --num_timesteps=1e7 --save_path=models/da_10M_acktr 
