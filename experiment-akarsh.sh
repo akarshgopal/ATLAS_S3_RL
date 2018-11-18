38f4d03232966459f6e7a82bc6a2f560b6e4755e
AkarshG1 <akarsh98@gmail.com>
2018-11-18 21:49:29 +0100

    trained si-acktr on pong 10M steps

--- experiment-akarsh.sh
+++ experiment-akarsh.sh
@@ -28,15 +28,15 @@
 #OPENAI_LOGDIR=logs/da_10M_acktr python -m baselines.run --alg=acktr --env=DemonAttackNoFrameskip-v0 --num_timesteps=1e7 --save_path=models/da_10M_acktr 
 
 
-#echo "Training ppo2 on Qbert for 10M Steps"
-#OPENAI_LOGDIR=logs/qb_10M_ppo2 python -m baselines.run --alg=ppo2 --env=QbertNoFrameskip-v0 --num_timesteps=1e7 --save_path=models/qb_10M_ppo2 
+	#echo "Training ppo2 on Qbert for 10M Steps"
+	#OPENAI_LOGDIR=logs/qb_10M_ppo2 python -m baselines.run --alg=ppo2 --env=QbertNoFrameskip-v0 --num_timesteps=1e7 --save_path=models/qb_10M_ppo2 
 
-#echo "Training acktr on Qbert for 10M Steps"
-#OPENAI_LOGDIR=logs/qb_10M_acktr python -m baselines.run --alg=acktr --env=QbertNoFrameskip-v0 --num_timesteps=1e7 --save_path=models/qb_10M_acktr 
+	#echo "Training acktr on Qbert for 10M Steps"
+	#OPENAI_LOGDIR=logs/qb_10M_acktr python -m baselines.run --alg=acktr --env=QbertNoFrameskip-v0 --num_timesteps=1e7 --save_path=models/qb_10M_acktr 
 
-echo "Training ppo2-si on Pong for 10M Steps"
-OPENAI_LOGDIR=logs/si-pong_10M_ppo2 python -m baselines.run --alg=ppo2 --env=PongNoFrameskip-v0 --num_timesteps=1e7 --load_path=models/si_10M_ppo2 --save_path=models/si-pong_10M_ppo2 
+#echo "Training ppo2-si on Pong for 10M Steps"
+#OPENAI_LOGDIR=logs/si-pong_10M_ppo2 python -m baselines.run --alg=ppo2 --env=PongNoFrameskip-v0 --num_timesteps=1e7 --load_path=models/si_10M_ppo2 --save_path=models/si-pong_10M_ppo2 
 
-#echo "Training acktr-si on Pong for 10M Steps"
-#OPENAI_LOGDIR=logs/si-pong_10M_acktr python -m baselines.run --alg=acktr --env=PongNoFrameskip-v0 --num_timesteps=1e7 --load_path=models/si_10M_acktr --save_path=models/si-pong_10M_acktr 
+echo "Training acktr-si on Pong for 10M Steps"
+OPENAI_LOGDIR=logs/si-pong_10M_acktr python -m baselines.run --alg=acktr --env=PongNoFrameskip-v0 --num_timesteps=1e7 --load_path=models/si_10M_acktr --save_path=models/si-pong_10M_acktr 
 
