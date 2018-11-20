9016fb9e7659141dc82d6d90c7532aa8cac6c373
AkarshG1 <akarsh98@gmail.com>
2018-11-20 21:44:36 +0100

    updated experiment-akarsh.sh for 20/11

--- experiment-akarsh.sh
+++ experiment-akarsh.sh
@@ -19,18 +19,18 @@
 	#echo "Training ppo2 on Pong for 10M Steps"
 	#OPENAI_LOGDIR=logs/pong_10M_ppo2 python -m baselines.run --alg=ppo2 --env=PongNoFrameskip-v0 --num_timesteps=1e7 --save_path=models/pong_10M_ppo2 
 
-#Running 
+
 #19/11/18 Akarsh
-echo "Training acktr on Pong for 10M Steps"
-OPENAI_LOGDIR=logs/pong_10M_acktr_1 python -m baselines.run --alg=acktr --env=PongNoFrameskip-v0 --num_timesteps=1e7 --save_path=models/pong_10M_acktr_1 
+	#echo "Training acktr on Pong for 10M Steps"
+	#OPENAI_LOGDIR=logs/pong_10M_acktr_1 python -m baselines.run --alg=acktr --env=PongNoFrameskip-v0 --num_timesteps=1e7 --save_path=models/pong_10M_acktr_1 
 
 # 18/11/18 Akarsh
 	#echo "Training ppo2 on DemonAttack for 10M Steps"
 	#OPENAI_LOGDIR=logs/da_10M_ppo2 python -m baselines.run --alg=ppo2 --env=DemonAttackNoFrameskip-v0 --num_timesteps=1e7 --save_path=models/da_10M_ppo2 
 
 #19/11/18 Akarsh
-echo "Training acktr on DemonAttack for 10M Steps"
-OPENAI_LOGDIR=logs/da_10M_acktr_1 python -m baselines.run --alg=acktr --env=DemonAttackNoFrameskip-v0 --num_timesteps=1e7 --save_path=models/da_10M_acktr_1 
+	#echo "Training acktr on DemonAttack for 10M Steps"
+	#OPENAI_LOGDIR=logs/da_10M_acktr_1 python -m baselines.run --alg=acktr --env=DemonAttackNoFrameskip-v0 --num_timesteps=1e7 --save_path=models/da_10M_acktr_1 
 
 #SI Transfer
 # 18/11/18 Akarsh
@@ -40,6 +40,13 @@ OPENAI_LOGDIR=logs/da_10M_acktr_1 python -m baselines.run --alg=acktr --env=Demo
 	#echo "Training acktr-si on Pong for 10M Steps"
 	#OPENAI_LOGDIR=logs/si-pong_10M_acktr python -m baselines.run --alg=acktr --env=PongNoFrameskip-v0 --num_timesteps=1e7 --load_path=models/si_10M_acktr --save_path=models/si-pong_10M_acktr 
 
+
+#Running 
+echo "Training ppo2-si on Pong for 10M Steps"
+OPENAI_LOGDIR=logs/si-pong_10M_ppo2_2 python -m baselines.run --alg=ppo2 --env=PongNoFrameskip-v0 --num_timesteps=1e7 --load_path=models/si_10M_ppo2 --save_path=models/si-pong_10M_ppo2_2 
+
+echo "Training acktr-si on Pong for 10M Steps"
+OPENAI_LOGDIR=logs/si-pong_10M_acktr_2 python -m baselines.run --alg=acktr --env=PongNoFrameskip-v0 --num_timesteps=1e7 --load_path=models/si_10M_acktr --save_path=models/si-pong_10M_acktr_2 
 
 #TODO
 #echo "Training ppo2-si on DemonAttack for 10M Steps"
@@ -51,3 +58,5 @@ OPENAI_LOGDIR=logs/da_10M_acktr_1 python -m baselines.run --alg=acktr --env=Demo
 #echo "Training acktr-si on Pong for 10M Steps"
 #OPENAI_LOGDIR=logs/si-da_10M_acktr python -m baselines.run --alg=acktr --env=DemonAttackNoFrameskip-v0 --num_timesteps=1e7 --load_path=models/si_10M_acktr --save_path=models/si-da_10M_acktr 
 
+#echo "Training ppo2-si on DemonAttack for 10M Steps R2"
+#OPENAI_LOGDIR=logs/si-da_10M_ppo2_2 python -m baselines.run --alg=ppo2 --env=DemonAttackNoFrameskip-v0 --num_timesteps=1e7 --load_path=models/si_10M_ppo2 --save_path=models/si-da_10M_ppo2_2 
