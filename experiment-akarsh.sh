db8fe69f31d01a46a6d9b44264ce55d8e71f9160
AkarshG1 <akarsh98@gmail.com>
2018-11-23 08:31:16 +0100

    trained ppo2-si on qbert x 2

--- experiment-akarsh.sh
+++ experiment-akarsh.sh
@@ -48,14 +48,20 @@
 	#OPENAI_LOGDIR=logs/si-pong_10M_acktr_2 python -m baselines.run --alg=acktr --env=PongNoFrameskip-v0 --num_timesteps=1e7 --load_path=models/si_10M_acktr --save_path=models/si-pong_10M_acktr_2 
 
 
-#Running
 #21/11/18
-#echo "Training acktr-si on DemonAttack for 10M Steps"
-#OPENAI_LOGDIR=logs/si_da_10M_acktr python -m baselines.run --alg=acktr --env=DemonAttackNoFrameskip-v0 --num_timesteps=1e7 --load_path=models/si_10M_acktr --save_path=models/si_da_10M_acktr 
+	#echo "Training acktr-si on DemonAttack for 10M Steps"
+	#OPENAI_LOGDIR=logs/si_da_10M_acktr python -m baselines.run --alg=acktr --env=DemonAttackNoFrameskip-v0 --num_timesteps=1e7 --load_path=models/si_10M_acktr --save_path=models/si_da_10M_acktr 
 
-#echo "Training acktr-si on DemonAttack for 10M Steps R2"
-#OPENAI_LOGDIR=logs/si_da_10M_acktr_2 python -m baselines.run --alg=acktr --env=DemonAttackNoFrameskip-v0 --num_timesteps=1e7 --load_path=models/si_10M_acktr --save_path=models/si_da_10M_acktr_2 
+	#echo "Training acktr-si on DemonAttack for 10M Steps R2"
+	#OPENAI_LOGDIR=logs/si_da_10M_acktr_2 python -m baselines.run --alg=acktr --env=DemonAttackNoFrameskip-v0 --num_timesteps=1e7 --load_path=models/si_10M_acktr --save_path=models/si_da_10M_acktr_2 
 
+#Running
+#22/11/18
+echo "Training ppo2-si on QBert for 10M Steps"
+OPENAI_LOGDIR=logs/si_qb_10M_ppo2_1 python -m baselines.run --alg=ppo2 --env=DemonAttackNoFrameskip-v0 --num_timesteps=1e7 --load_path=models/si_10M_ppo2 --save_path=models/si_da_10M_ppo2_1 
+
+echo "Training ppo2-si on Qbert for 10M Steps R2"
+OPENAI_LOGDIR=logs/si_qb_10M_ppo2_2 python -m baselines.run --alg=ppo2 --env=DemonAttackNoFrameskip-v0 --num_timesteps=1e7 --load_path=models/si_10M_ppo2 --save_path=models/si_da_10M_ppo2_2 
 
 #TODO
 #echo "Training ppo2-si on DemonAttack for 10M Steps"
