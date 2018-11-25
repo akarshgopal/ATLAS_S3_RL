00fb5da79d139c87b5900256a75134eb5385183e
lmbguy <lmb-vdneut@hotmail.com>
2018-11-25 18:13:00 +0100

    trained on SpaceInvaders init QB

--- experiments_l/11-24.sh
+++ experiments_l/11-24.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo "Training ACKTR on SpaceInvaders using QB init for 10M Steps"
-OPENAI_LOGDIR=logs/qb_si_10M_acktr_1_recovery python -m baselines.run --alg=acktr --env=SpaceInvadersNoFrameskip-v0 --num_timesteps=1e7 --load_path=models/qb_10M_acktr --save_path=models/qb_si_10M_acktr_1
+OPENAI_LOGDIR=logs/qb_si_10M_acktr_1_recovery_2 python -m baselines.run --alg=acktr --env=SpaceInvadersNoFrameskip-v0 --num_timesteps=1e7 --load_path=models/qb_10M_acktr --save_path=models/qb_si_10M_acktr_1
 
 echo "Training ACKTR on SpaceInvaders using QB init for 10M Steps run 2"
-OPENAI_LOGDIR=logs/qb_si_10M_acktr_2_recovery python -m baselines.run --alg=acktr --env=SpaceInvadersNoFrameskip-v0 --num_timesteps=1e7 --load_path=models/qb_10M_acktr --save_path=models/qb_si_10M_acktr_2
+OPENAI_LOGDIR=logs/qb_si_10M_acktr_2_recovery_2 python -m baselines.run --alg=acktr --env=SpaceInvadersNoFrameskip-v0 --num_timesteps=1e7 --load_path=models/qb_10M_acktr --save_path=models/qb_si_10M_acktr_2
 
 #
 git pull
