38a73019c48433dda615479100281038da112ae9
lmbguy <lmb-vdneut@hotmail.com>
2018-11-26 07:36:27 +0100

    trained on pong init QB

--- experiments_l/11-26.sh
+++ experiments_l/11-26.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Training ACKTR on DemonAttack using QB init for 10M Steps"
-OPENAI_LOGDIR=logs/qb_da_10M_acktr_1 python -m baselines.run --alg=acktr --env=SpaceInvadersNoFrameskip-v0 --num_timesteps=1e7 --load_path=models/qb_10M_acktr --save_path=models/qb_da_10M_acktr_1
+OPENAI_LOGDIR=logs/qb_da_10M_acktr_1 python -m baselines.run --alg=acktr --env=DemonAttackNoFrameskip-v0 --num_timesteps=1e7 --load_path=models/qb_10M_acktr --save_path=models/qb_da_10M_acktr_1
 
 echo "Training ACKTR on DemonAttack using QB init for 10M Steps run 2"
 OPENAI_LOGDIR=logs/qb_da_10M_acktr_2 python -m baselines.run --alg=acktr --env=DemonAttackNoFrameskip-v0 --num_timesteps=1e7 --load_path=models/qb_10M_acktr --save_path=models/qb_da_10M_acktr_2
