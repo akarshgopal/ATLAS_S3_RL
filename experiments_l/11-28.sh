1653420dc5e9d641c050c3dc6db62f3a50c9e0cc
lmbguy <lmb-vdneut@hotmail.com>
2018-12-02 21:36:48 +0100

    modified:   experiments_l/11-28.sh 	modified:   experiments_l/11-29.sh 	modified:   experiments_l/11-30.sh 	modified:   experiments_l/12-01.sh

--- experiments_l/11-28.sh
+++ experiments_l/11-28.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-echo "Training ACKTR on QB using pong init for 10M Steps"
-OPENAI_LOGDIR=logs/pong_qb_10M_acktr_1 python -m baselines.run --alg=acktr --env=QbertNoFrameskip-v0 --num_timesteps=1e7 --load_path=models/pong_10M_acktr_1 --save_path=models/pong_qb_10M_acktr_1
+#echo "Training ACKTR on QB using pong init for 10M Steps"
+#OPENAI_LOGDIR=logs/pong_qb_10M_acktr_1 python -m baselines.run --alg=acktr --env=QbertNoFrameskip-v0 --num_timesteps=1e7 --load_path=models/pong_10M_acktr_1 --save_path=models/pong_qb_10M_acktr_1
 
-echo "Training ACKTR on QB using pong init for 10M Steps run 2"
-OPENAI_LOGDIR=logs/pong_qb_10M_acktr_2 python -m baselines.run --alg=acktr --env=QbertNoFrameskip-v0 --num_timesteps=1e7 --load_path=models/pong_10M_acktr_1 --save_path=models/pong_qb_10M_acktr_2
+#echo "Training ACKTR on QB using pong init for 10M Steps run 2"
+#OPENAI_LOGDIR=logs/pong_qb_10M_acktr_2 python -m baselines.run --alg=acktr --env=QbertNoFrameskip-v0 --num_timesteps=1e7 --load_path=models/pong_10M_acktr_1 --save_path=models/pong_qb_10M_acktr_2
 
-#echo "Training ACKTR on DA using pong init for 10M Steps"
-OPENAI_LOGDIR=logs/pong_da_10M_acktr_1 python -m baselines.run --alg=acktr --env=DemonAttackNoFrameskip-v0 --num_timesteps=1e7 --load_path=models/pong_10M_acktr_1 --save_path=models/pong_da_10M_acktr_1
+echo "Training ACKTR on DA using pong init for 10M Steps"
+OPENAI_LOGDIR=logs/pong_da_10M_acktr_1_recovery python -m baselines.run --alg=acktr --env=DemonAttackNoFrameskip-v0 --num_timesteps=1e7 --load_path=models/pong_10M_acktr_1 --save_path=models/pong_da_10M_acktr_1_recovery
 
-#echo "Training ACKTR on DA using pong init for 10M Steps run 2"
-OPENAI_LOGDIR=logs/pong_da_10M_acktr_2 python -m baselines.run --alg=acktr --env=DemonAttackNoFrameskip-v0 --num_timesteps=1e7 --load_path=models/pong_10M_acktr_1 --save_path=models/pong_da_10M_acktr_2
+echo "Training ACKTR on DA using pong init for 10M Steps run 2"
+OPENAI_LOGDIR=logs/pong_da_10M_acktr_2_recovery python -m baselines.run --alg=acktr --env=DemonAttackNoFrameskip-v0 --num_timesteps=1e7 --load_path=models/pong_10M_acktr_1 --save_path=models/pong_da_10M_acktr_2_recovery
 
 #
 git pull
